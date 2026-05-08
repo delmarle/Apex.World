8e431cf32f2d13091036b241ad0c8ce924315937
damien <delmarle.damien@gmail.com>
2026-05-08 10:57:47 +0200

    initial commit

---
+++ Libraries/ApexWorld/Code/ApexWorldManager.cs
@@ -0,0 +1,10 @@
+using Sandbox;
+
+/// <summary>
+/// This need to be a root component of everything it manager, terrains, gameObjects ...
+/// </summary>
+[Title( "Apex World - Manager" )]
+public class ApexWorldManager : Component
+{
+
+}
---
+++ Libraries/ApexWorld/Code/Spawners/BaseSpawner.cs
@@ -0,0 +1,11 @@
+﻿namespace Sandbox.Spawners;
+
+public abstract class BaseSpawner: Component
+{
+	[Property] public string SpawnerName { get; set; }
+
+	public override string ToString()
+	{
+		return $"{GetType().Name} - {SpawnerName}";
+	}
+}
